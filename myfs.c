c0ff87ac8e170061f9270996483fdc80e0150460
Endrew Jonathan <42884284+Entreprise005@users.noreply.github.com>
2023-11-23 22:14:14 -0400

    Novas funções implementadas

--- myfs.c
+++ myfs.c
@@ -4,6 +4,10 @@
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
 
 #define FILE_PATH "/Documentos/TesteMakefile"
 
@@ -28,14 +32,34 @@
 
 static struct stat regular_file = {.st_mode = S_IFREG | 00400};
 
-int my_getattr (const char *path, struct stat *s, struct fuse_file_info *fInfo){
+int my_getattr (const char *path, struct stat *s){
 
-	if(!strcmp(path, "/")){
+	/*if(!strcmp(path, "/")){
 		s->st_mode = S_IFDIR | 00400;
 	}
 	if(!strcmp(path, "/my_file")){
 		s->st_mode = regular_file.st_mode;
 		s->st_size = strlen("Arquivo\n");
+	}*/
+	
+	printf( "[getattr] Called\n");
+	printf("Attributes of %s requested\n", path);
+	
+	s->st_uid = getuid();
+	s->st_gid = getgid();
+	s->st_atime = time(NULL);
+	s->st_mtime = time (NULL);
+	
+	if (strcmp(path, "/") == 0)
+	{
+		s->st_mode = S_IFDIR | 0755;
+		s->st_nlink = 2;
+	}
+	else
+	{
+		s->st_mode = S_IFREG | 0644;
+		s->st_nlink = 1;
+		s->st_size = 1024;
 	}
 	
 	return 0;
@@ -46,17 +70,35 @@ int my_opendir (const char *, struct fuse_file_info *fInfo){
 	return 0;
 }
 
-int my_readdir (const char *path, void *buf, fuse_fill_dir_t filler, off_t offset,
+int my_readdir (const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset,
 			struct fuse_file_info *fInfo){
 	
-	static struct stat regular_file = {.st_mode = S_IFREG | 00400};
+	/*static struct stat regular_file = {.st_mode = S_IFREG | 00400};
     	if(!strcmp(path, "/")){
 		filler(buf, "my_file", &regular_file, 0);
+	}*/
+	
+	printf( "--> Getting The List of Files of %s\n", path );
+	
+	filler( buffer, ".", NULL, 0 ); // Current Directory
+	filler( buffer, "..", NULL, 0 ); // Parent Directory
+	
+	if ( strcmp( path, "/" ) == 0 ) // If the user is trying to show the files/directories of the 			root directory show the following
+	{
+		filler( buffer, "file54", NULL, 0 );
+		filler( buffer, "file349", NULL, 0 );
 	}
 	return 0;			
 }
 
-int my_mkdir (const char *, mode_t){
+int my_mkdir (const char *path, mode_t mode){
+	
+	int res;
+
+	res = mkdir(path, mode);
+	if (res == -1)
+		return -errno;
+	
 	return 0;
 }
 
@@ -65,9 +107,13 @@ int my_rmdir (const char *){
 }
 
 int my_openFile (const char *path, struct fuse_file_info *fInfo){
-	(void) path;
-	
-	fInfo->keep_cache = 1;
+	int res;
+
+	res = open(path, fInfo->flags);
+	if (res == -1)
+		return -errno;
+
+	fInfo->fh = res;
 	return 0;
 }
 
@@ -100,10 +146,37 @@ int my_readFile (const char *path, char *buffer, size_t size, off_t offset,
     return bytesRead;
 }
 
-int my_writeFile (const char *path, const char *, size_t size, off_t offset,
+int my_writeFile (const char *path, const char *buffer, size_t size, off_t offset,
 		      struct fuse_file_info *fInfo){
-		      
-		      return 0;
+		(void) fInfo;
+		
+		char filePath[255];
+		snprintf(filePath, sizeof(filePath), "%s%s", "~/Documentos/TesteMakefile/", path);
+		
+		int fd = open(filePath, O_WRONLY);
+		if (fd == -1){
+			perror("ERRO ao abrir o arquivo para escrita\n");
+			return -errno;
+		}
+		// Posiciona o cursor no offset correto no arquivo
+    	if (lseek(fd, offset, SEEK_SET) == -1) {
+        	perror("Erro ao posicionar o cursor no arquivo");
+        	close(fd);
+        	return -errno;
+    	}
+
+    	// Escreve os dados no arquivo
+    	ssize_t bytesWritten = write(fd, buffer, size);
+    	if (bytesWritten == -1) {
+        	perror("Erro ao escrever dados no arquivo");
+        	close(fd);
+        	return -errno;
+    	}
+
+    	// Fecha o arquivo
+    	close(fd);
+
+    	return bytesWritten;
 }
 
 static struct fuse_operations operations = {
@@ -121,4 +194,3 @@ int main(int argc, char *argv[])
 {
 	return fuse_main(argc, argv, &operations, NULL);
 }
-
